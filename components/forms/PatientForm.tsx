ac15456ec270dd41205e0836b4df1ae798dab707
developerthierry <developerthierry@gmail.com>
2026-01-04 01:02:18 +0000

    implement create user

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -9,6 +9,7 @@ import SubmitButton from "../SubmitButton";
 import { useState } from "react";
 import { UserFormValidation } from "@/lib/validation";
 import { useRouter } from "next/navigation";
+import { createUser } from "@/lib/actions/patient.actions";
 
 export enum FormFieldType {
   INPUT = "input",
@@ -35,13 +36,13 @@ const PatientForm = () => {
   async function onSubmit({name, email, phone}: z.infer<typeof UserFormValidation>) {
     setIsLoading(true)
     try{
-      // const userData={
-      //   name,
-      //   email,
-      //   phone
-      // }
-      // const user = await createUser(userData)
-      // if(user)router.push(`/patients//${user.$id}/register`)
+      const userData={
+        name,
+        email,
+        phone
+      }
+      const user = await createUser(userData)
+      if(user)router.push(`/patients//${user.$id}/register`)
     }
     catch(err){
       console.log(err)
